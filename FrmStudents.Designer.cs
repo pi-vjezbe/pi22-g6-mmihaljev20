f8ce018d0d9624c18bccd35e24d18d3c97e9c9fd
ProgramskoInzenjerstvo <pi_foi@outlook.com>
2022-05-02 21:00:27 +0200

    Implemetiran prikaz liste studenata.

--- FrmStudents.Designer.cs
+++ FrmStudents.Designer.cs
@@ -28,19 +28,34 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(73, 28);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(630, 373);
+            this.dataGridView1.TabIndex = 0;
             // 
             // FrmStudents
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dataGridView1);
             this.Name = "FrmStudents";
             this.Text = "FrmStudents";
+            this.Load += new System.EventHandler(this.FrmStudents_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
     }
 }
--- FrmStudents.cs
+++ FrmStudents.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Evaluation_Manager.Models;
+using Evaluation_Manager.Repositories;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,9 +14,27 @@ namespace Evaluation_Manager
 {
     public partial class FrmStudents : Form
     {
+        private object dgvStudents;
+
         public FrmStudents()
         {
             InitializeComponent();
+        }
+
+        private void FrmStudents_Load(object sender, EventArgs e)
+        {
+           ShowStudents();
+        }
+        private void ShowStudents()
+        {
+            List<Student> students = StudentRepository.GetStudents();
+            dataGridView1.DataSource = students;
+            dataGridView1.Columns["Id"].DisplayIndex = 0;
+            dataGridView1.Columns["FirstName"].DisplayIndex = 1;
+            dataGridView1.Columns["LastName"].DisplayIndex = 2;
+            dataGridView1.Columns["Grade"].DisplayIndex = 3;
+
+
         }
     }
 }
